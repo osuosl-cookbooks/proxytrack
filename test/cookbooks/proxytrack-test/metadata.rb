ff3a32976f844904671c5ef41fd44e7868409b7d
Robert Detjens <detjensrobert@osuosl.org>
2020-06-18 18:18:29 -0700

    cookstyle fixes

--- test/cookbooks/proxytrack-test/metadata.rb
+++ test/cookbooks/proxytrack-test/metadata.rb
@@ -6,7 +6,6 @@ chef_version     '>= 13'
 issues_url       'https://github.com/osuosl-cookbooks/proxytrack-test/issues'
 source_url       'https://github.com/osuosl-cookbooks/proxytrack-test'
 description      'Installs/Configures proxytrack-test'
-long_description 'Installs/Configures proxytrack-test'
 version          '0.1.0'
 
 supports         'centos', '~> 7.0'

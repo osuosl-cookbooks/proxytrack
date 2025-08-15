9f1b669455db53e374d2d2dc0f8407e5996cd39a
Ankit Malhotra <ankitmalhotra788@gmail.com>
2025-08-15 14:50:03 -0700

    Almalinux 10 Support

--- metadata.rb
+++ metadata.rb
@@ -10,6 +10,7 @@ version          '1.4.1'
 
 supports         'almalinux', '~> 8.0'
 supports         'almalinux', '~> 9.0'
+supports         'almalinux', '~> 10.0'
 
 depends          'yum-epel'
 depends          'yum-osuosl'

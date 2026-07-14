d86b912ff83cd067f51e72023ce7438340313e9d
Manatee <github@osuosl.org>
2026-07-14 00:21:09 +0000

    Automatic patch-level version bump to v1.4.3 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ proxytrack CHANGELOG
 This file is used to list changes made in each version of the
 proxytrack cookbook.
 
+1.4.3 (2026-07-14)
+------------------
+- cinc: updated kitchen and dokken files
+
 1.4.2 (2025-08-18)
 ------------------
 - Almalinux 10 Support
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ chef_version     '>= 16'
 issues_url       'https://github.com/osuosl-cookbooks/proxytrack/issues'
 source_url       'https://github.com/osuosl-cookbooks/proxytrack'
 description      'Installs/Configures proxytrack'
-version          '1.4.2'
+version          '1.4.3'
 
 supports         'almalinux', '~> 8.0'
 supports         'almalinux', '~> 9.0'
